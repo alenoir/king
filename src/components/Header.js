c4c76c7ec24a8d450d1014f6af0c1301eeebca61
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-09-13 16:08:37 +0200

    integration feed

---
+++ src/components/Header.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import ReactNative from 'react-native';
+
+const {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+} = ReactNative;
+
+const {
+  PropTypes,
+  Component,
+} = React;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  title: {
+    flex: 4,
+    fontFamily: 'Montserrat-Black',
+    fontSize: 16,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+  titleBig: {
+    fontSize: 26,
+  },
+});
+
+class Header extends Component {
+
+  renderRightButton() {
+    const { onRightButtonPress, buttonRightImage } = this.props;
+
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={(onRightButtonPress)}
+      >
+        <Image
+          style={styles.closeButtonIcon}
+          source={buttonRightImage}
+        />
+      </TouchableOpacity>
+    );
+  }
+  render() {
+    const {
+      onRightButtonPress,
+      onLeftButtonPress,
+      buttonLeftImage,
+      buttonRightImage,
+      big,
+    } = this.props;
+
+    const styleTitle = [styles.title];
+    if (big) {
+      styleTitle.push(styles.titleBig);
+    }
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={(onLeftButtonPress)}
+        >
+          <Image
+            style={styles.closeButtonIcon}
+            source={buttonLeftImage}
+          />
+        </TouchableOpacity>
+        <Text style={styleTitle}>{this.props.title}</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={(onRightButtonPress)}
+        >
+          <Image
+            style={styles.closeButtonIcon}
+            source={buttonRightImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+    );
+  }
+
+}
+
+Header.propTypes = {
+  big: PropTypes.bool,
+  title: PropTypes.object,
+  onLeftButtonPress: PropTypes.object,
+  onRightButtonPress: PropTypes.object,
+  buttonLeftImage: PropTypes.object,
+  buttonRightImage: PropTypes.object,
+};
+
+module.exports = Header;
